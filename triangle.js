6102c7a5b134abee04f48e3aa7e4e599f133064a
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2025-09-21 20:02:55 -0600

    init, two triangles + random colors + rotation

---
+++ triangle.js
@@ -0,0 +1,18 @@
+// --- Triangle ---
+const positions = new Float32Array([
+   -1.0, 0.0, 0.0,  // vertex 1
+   -0.5, .866, 0.0,  // vertex 2 (root 3 over 2 height)
+   0.0, 0.0, 0.0,   // vertex 3
+   1.0, 0.0, 0.0,  // vertex 1
+   0.5, -0.866, 0.0,  // vertex 2
+   0.0, 0.0, 0.0   // vertex 3
+]);
+
+var colors = new Float32Array([
+  1.0, 1.0, 1.0,  // White
+  1.0, 1.0, 1.0,  // White
+  1.0, 1.0, 1.0,   // White
+  1.0, 1.0, 1.0,   // White
+  1.0, 1.0, 1.0,   // White
+  1.0, 1.0, 1.0   // White
+]);
